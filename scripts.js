f29056ca23455d0bb5605ceed3c10c36eea93b1c
SoftimusPro <darielanielys@gmail.com>
2025-01-30 15:56:06 -0500

    Funcionalidad del tiempo basica

--- scripts.js
+++ scripts.js
@@ -4,6 +4,7 @@ const fecha = document.querySelector(".fecha")
 const pronostico = document.querySelector(".pronostico")
 
 const apiUbicacion = '5dcc0d23edb9b9e60b9eedab244a073d'
+const apiTiempo  = '43f8f9d4ad403a05cb4aedca6b97e47a'
 
 function relojDigital() {
     let f = new Date()
@@ -26,8 +27,19 @@ async function obtenerUbicacion() {
     const url = `https://api.ipapi.com/api/check?access_key=${apiUbicacion}`;
     const response = await fetch(url);
     const data = await response.json();
+    const latitudeIp = data.latitude
+    const longitudeIp = data.longitude
     console.log(data); // Muestra los datos de ubicación en la consola
-    pronostico.innerHTML = `El tiempo en: ${data.city}, ${data.region_name}, ${data.country_name} para hoy es:`;
+    pronostico.innerHTML = `El tiempo en ${data.city}, ${data.region_name}, ${data.country_name} para hoy es: <br></br>`;
+    obtenerTiempo(latitudeIp, longitudeIp)
+}
+
+async function obtenerTiempo(latitudeIp, longitudeIp) {
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitudeIp}&lon=${longitudeIp}&units=metric&appid=${apiTiempo}&lang=es`
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data); // Muestra los datos del tiempo en la consola
+        pronostico.innerHTML += ` ${data.weather[0].description}, ${data.main.temp}°C`;
 }
 
 setInterval(() => {
